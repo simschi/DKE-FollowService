89487cd15078e2905faca676b1e3af29419c01d7
Simon Schiffer <simon.schiffer@gmx.at>
2021-04-29 11:23:24 +0200

    Init spring project

---
+++ src/main/java/pr/DKEFollowService/DkeFollowServiceApplication.java
@@ -0,0 +1,13 @@
+package pr.DKEFollowService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkeFollowServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkeFollowServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pr/DKEFollowService/DkeFollowServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pr.DKEFollowService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkeFollowServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
